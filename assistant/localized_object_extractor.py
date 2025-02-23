d7e535e03f84d936ec54352983464a4ed24d84c6
Nimit <nimit.shah622@gmail.com>
2025-02-22 19:15:00 -0500

    text to speech & AI translation capabilities

---
+++ assistant/localized_object_extractor.py
@@ -0,0 +1,80 @@
+import sys
+import json
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from load_dotenv import load_dotenv
+
+class LocalizedData:
+  object: str
+  translated_classes: list[str]
+  instructions: list[str]
+
+  def __init__(self, object, translated_classes, instructions):
+    self.object = object
+    self.locale_object = translated_classes[object] if object in translated_classes else object
+    self.translated_classes = translated_classes
+    self.instructions = instructions
+  
+  def get(self, key, obstacle=None):
+    object = obstacle if obstacle else (self.locale_object if self.locale_object else "")
+    return self.instructions.get(key, "Localization Error").replace("{{object}}", object)
+
+def extract_object(input_text, language, instructions):
+  load_dotenv()
+  sys.stdout.reconfigure(encoding='utf-8')
+
+  # Initialize the model
+  llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+  # Create a prompt template
+  prompt_template = PromptTemplate.from_template("""You are an intelligent chatbot that processes user input to identify objects from a predefined list of objects, translate text, and return a structured JSON output. Follow these steps precisely:
+
+  Translate {input} in english and determine if it contains any object or its synonym which is mentioned in the following list:
+  [person, bicycle, car, motorcycle, airplane, bus, train, truck, boat, traffic light, fire hydrant, stop sign, parking meter, bench, bird, cat, dog, horse, sheep, cow, elephant, bear, zebra, giraffe, backpack, umbrella, handbag, tie, suitcase, frisbee, skis, snowboard, sports ball, kite, baseball bat, baseball glove, skateboard, surfboard, tennis racket, bottle, wine glass, cup, fork, knife, spoon, bowl, banana, apple, sandwich, orange, brocolli, carrot, hot dog, pizza, donut, cake, chair, couch, potted plant, bed, dining table, toilet, tv, laptop, mouse, remote, keyboard, cell phone, microwave, oven, toaster, sink, refrigerator, book, clock, vase, scissors, teddy bear, hair drier, toothbrush]
+  If an object is found, return its English name. Otherwise, return none.
+  Translate the entire list of 80 objects into the {language} language.
+  Translate instructions: {instructions} into the {language} language.
+  Format the response as JSON with the following structure:
+  {{
+    "identified_object": "<object_in_english_or_none>",
+    "translated_objects": {{
+      "person": "<translated_word>",
+      "bicycle": "<translated_word>",
+      ...
+    }},
+    "instructions": "<translated_instructions>"
+  }}
+  If no object is found, set "identified_object": "none" and "translated_objects": null, but still translate the instructions.
+
+  Ensure accuracy in translations and maintain JSON structure integrity.
+
+  Additional note to take care when translating the instructions, look for double curly braces {{}} in the instructions and consider the text between them as a variable so translate it accordingly that it makes sense if i replace the variable with the object in future.""")
+
+  prompt = prompt_template.format(input=input_text, language=language, instructions=instructions)
+
+  # Invoke the model with the prepared prompt
+  response = llm.invoke(prompt)
+
+  json_string = response.content
+  json_string = json_string[8:] if json_string.startswith("```json") else json_string 
+  json_string = json_string[:-3] if json_string.endswith("```") else json_string
+  data = json.loads(json_string.strip())
+  object = data['identified_object']
+  translated_classes = data['translated_objects'] if data['translated_objects'] else {}
+  instructions = data['instructions']
+
+  return LocalizedData(object if object != "none" else None, translated_classes, instructions)
+
+if __name__ == "__main__":
+  #? Test this GenAI pipeline
+  input_text = "मैं अपनी बोतल ढूंढना चाहता हूँ"
+  language = "hi" #* hindi language tag
+  instructions = {
+    "move_left": "Turn left.",
+    "move_right": "Turn right.",
+    "move_obstructed": "There is a {{object}} in front of you, turn left",
+  }
+  loc_data = extract_object(input_text, language, instructions)
+  print(loc_data.object)
+  print(len(loc_data.translated_classes))
+  print(loc_data.instructions)
---
+++ assistant/whisper.py
@@ -0,0 +1,42 @@
+import sounddevice as sd
+import torch
+from transformers import pipeline
+import warnings
+from langdetect import detect_langs
+from localized_object_extractor import extract_object, LocalizedData
+from utils import speak
+from constants import STRINGS, CLASSES
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+whisper = pipeline("automatic-speech-recognition", model="openai/whisper-medium", torch_dtype=torch.float32, device="cpu", use_fast=True)
+sample_rate = 16000
+
+def record_audio(record_duration):
+    print("Recording... Please speak.")
+    audio = sd.rec(int(record_duration * sample_rate), samplerate=sample_rate, device =1, channels=1, dtype="float32")
+    sd.wait()
+    print("Recording complete.")
+    return audio.flatten()
+
+def transcribe_audio(audio_data):
+    print("Transcribing audio...")
+    # transcription = whisper({"array": audio_data, "sampling_rate": sample_rate, "language": "en"})
+    transcription = whisper({"array": audio_data, "sampling_rate": sample_rate})
+    transcribed_text = transcription["text"]
+    print("Transcribed text:", transcribed_text)
+    try:
+        lang = detect_langs(transcribed_text)[0].lang
+        print("Detected language as", lang)
+    except Exception as err:
+        lang = "en"
+        print("Error while detecting language", err)
+    return extract_object(transcribed_text, lang, STRINGS), str(lang)
+
+def get_target_object(record_duration):
+    #* TEST
+    return LocalizedData("cup", CLASSES, STRINGS), "en"
+
+    audio_data = record_audio(record_duration)
+    speak(STRINGS['wait_while_load'])
+    loc_data, language = transcribe_audio(audio_data)
+    return loc_data, language
